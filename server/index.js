e1cf5a9a3e99d7694e09ebe5011ef901c2e5205f
taerikor <akdud153@gmail.com>
2021-11-11 14:26:28 +0900

    Fix for Express version update

--- server/index.js
+++ server/index.js
@@ -1,35 +1,30 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-const config = require('./config/key');
+const config = require("./config/key");
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("mongoDB Connected"))
+  .catch((err) => console.log(err));
 
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI, {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('mongoDB Connected'))
-.catch((err) => console.log(err))
+app.use(cors());
 
-app.use(cors())
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/product", require("./routes/product"));
 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/product', require('./routes/product'))
+app.use("/uploads", express.static("uploads"));
 
-app.use('/uploads', express.static('uploads'));
-
-
-
-const port = 5000
+const port = 5000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
